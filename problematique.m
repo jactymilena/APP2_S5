bd1c424fded444db67f276df68e76c08f7244b39
Jacty Saenz <47018467+jactymilena@users.noreply.github.com>
2023-01-25 11:34:15 -0500

    Correct p error

--- problematique.m
+++ problematique.m
@@ -10,14 +10,16 @@ x = [0 8 15 20 25 ];
 y = [ 30 19 20 16 15 ];
 n = 4;
 
-[p,S] = polyfit(x,y,n);
+[p,S] = polyfit(x,y,n)
 
 x1 = linspace(0,25);
-p = polyval(p,x1);
+y1 = polyval(p,x1);
 figure
 plot(x,y,'o')
 hold on
 plot(x1,y1)
 hold off
+
+
 
 
