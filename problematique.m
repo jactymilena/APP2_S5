bbc91affcfab08fe0fffa9ef96274300f025e2bc
Jacty Saenz <47018467+jactymilena@users.noreply.github.com>
2023-01-25 13:29:53 -0500

    Added quadratic error, rmse and coussin-trampoline

--- problematique.m
+++ problematique.m
@@ -26,7 +26,7 @@ x = [0:dx:100];
 y = [ 0.87 0.78 0.71 0.61 0.62 0.51 0.51 0.49 0.46 0.48 0.46 ];
 n = 2;
 
-[p,S] = polyfit(x,y,n);
+[p,S] = polyfit(x,y,n)
 
 x1 = linspace(0,100);
 y1 = polyval(p,x1);
@@ -35,3 +35,26 @@ plot(x,y,'o')
 hold on
 plot(x1,y1)
 hold off
+
+g = 0.0001*x.^2 - 0.0092*x + 0.8668;
+
+% Erreur quadratique
+E = sum((g-y).^2)
+E2 = (g-y)'*(g-y);
+
+% RMSE
+err_rms = sqrt(mean((g-y).*(g-y)))
+
+%% Coussin-trampoline
+
+mg = 9.81*80;
+h0 = 5;
+hr = 0;
+k = 6000;
+
+a = (1/2)*k;
+b = mg;
+c = -mg*h0;
+
+hf1 = (-b + sqrt((b^2)-(4*a*c)))/(2*a)
+hf2 = (-b - sqrt((b^2)-(4*a*c)))/(2*a)
