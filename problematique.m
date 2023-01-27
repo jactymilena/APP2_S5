64f5a11e2442e9ba2200b7ade1eaabb1a12c824a
Jacty Saenz <47018467+jactymilena@users.noreply.github.com>
2023-01-27 17:46:17 -0500

    Update problematique.m

--- problematique.m
+++ problematique.m
@@ -8,17 +8,31 @@ close all % vide fenetres et graphiques
 
 x = [0 8 15 20 25 ];
 y = [ 30 19 20 16 15 ];
-n = 4;
+n = 5;
 
-[p,S] = polyfit(x,y,n);
+p = [ x(1)^4 x(1)^3 x(1)^2 x(1) 1;
+      x(2)^4 x(2)^3 x(2)^2 x(2) 1;
+      x(3)^4 x(3)^3 x(3)^2 x(3) 1;
+      x(4)^4 x(4)^3 x(4)^2 x(4) 1;
+      x(5)^4 x(5)^3 x(5)^2 x(5) 1 ]
 
-x1 = linspace(0,25);
-y1 = polyval(p,x1);
-figure
-plot(x,y,'o')
-hold on
-plot(x1,y1)
-hold off
+coeff = pinv(p)*y'
+
+r = roots([coeff(5)*4 coeff(4)*3 coeff(3)*2 coeff(2)])
+
+ 
+
+%[p,S] = polyfit(x,y,n);
+
+%x1 = linspace(0,25);
+%y1 = polyval(p,x1);
+%figure
+%plot(x,y,'o')
+%hold on
+%plot(x1,y1)
+%hold off
+ 
+
 
 %% Trajectoire - polynome d'approximation
 dx = 10;
@@ -58,3 +72,38 @@ c = -mg*h0;
 
 hf1 = (-b + sqrt((b^2)-(4*a*c)))/(2*a)
 hf2 = (-b - sqrt((b^2)-(4*a*c)))/(2*a)
+
+%% Ballon-mousse et minuterie
+
+vb = -1;
+mb = 8;
+coeff_res = 0;
+
+%% Bassin d'eau
+
+kf = 0.95;
+b = 47;
+g = 9.81;
+m = 80;
+mg = m*g;
+h1 = 10;
+vf = 1;
+
+% Vitesse initiale
+vi = sqrt(2*g*h1);
+
+% Point d'operation
+v0 = sqrt( (mg*(1-kf)) / b );
+
+% Constantes
+C = ( (g*(1-kf)) / v0 ) - ( b / m );
+K = ( (g*(1+kf)) / v0^2 ) - ( (b*v0) / m );
+
+% Vitesses
+dvi = vi - v0;
+dvf = vf - v0;
+
+% Hauteur z
+z = ( (dvf^2 - dvi^2) / 2*C ) * -K 
+
+
