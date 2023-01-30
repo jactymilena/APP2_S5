7f1b8a6b543c9b8183547121e41385454e7fc9ee
Jacty Saenz <47018467+jactymilena@users.noreply.github.com>
2023-01-30 09:40:06 -0500

    Update problematique.m

--- problematique.m
+++ problematique.m
@@ -10,51 +10,61 @@ xn = [0 8 15 20 25 ];
 yn = [ 30 19 20 16 12.7843 ];
 names = {'A', 'B', 'C', 'D', 'E'}
 
-p = [ 1 xn(1) xn(1)^2 xn(1)^3 xn(1)^4;
+P = [ 1 xn(1) xn(1)^2 xn(1)^3 xn(1)^4;
       1 xn(2) xn(2)^2 xn(2)^3 xn(2)^4;
       1 xn(3) xn(3)^2 xn(3)^3 xn(3)^4;
       1 xn(4) xn(4)^2 xn(4)^3 xn(4)^4;
       1 xn(5) xn(5)^2 xn(5)^3 xn(5)^4 ]
-      
 
-coeff = pinv(p'*p)*p'*yn'
+A = pinv(P)*yn'
 
-r = roots([coeff(5)*4 coeff(4)*3 coeff(3)*2 coeff(2)])
+r = roots([A(5)*4 A(4)*3 A(3)*2 A(2)])
 x = linspace(0,25);
-y = coeff(1) + coeff(2)*x + coeff(3)*x.^2 + coeff(4)*x.^3 + coeff(5)*x.^4;
+g = A(1) + A(2)*x + A(3)*x.^2 + A(4)*x.^3 + A(5)*x.^4;
 
 figure
 hold on
 plot(xn, yn, 'o')
-plot(x, y)
+plot(x, g)
 title("Polynôme d'interpolation")
 text(xn, yn+0.45, names)
 hold off
 
 %% Trajectoire - polynome d'approximation
 dx = 10;
-x = [0:dx:100];
-y = [ 0.87 0.78 0.71 0.61 0.62 0.51 0.51 0.49 0.46 0.48 0.46 ];
-n = 2;
+xn = [0:dx:100];
+yn = [ 0.87 0.78 0.71 0.61 0.62 0.51 0.51 0.49 0.46 0.48 0.46 ];
 
-[p,S] = polyfit(x,y,n)
+p = [ 1 xn(1) xn(1)^2 xn(1)^3 xn(1)^4 xn(1)^5 xn(1)^6 xn(1)^7;
+      1 xn(2) xn(2)^2 xn(2)^3 xn(2)^4 xn(2)^5 xn(2)^6 xn(2)^7;
+      1 xn(3) xn(3)^2 xn(3)^3 xn(3)^4 xn(3)^5 xn(3)^6 xn(3)^7;
+      1 xn(4) xn(4)^2 xn(4)^3 xn(4)^4 xn(4)^5 xn(4)^6 xn(4)^7;
+      1 xn(5) xn(5)^2 xn(5)^3 xn(5)^4 xn(5)^5 xn(5)^6 xn(5)^7 ]
 
-x1 = linspace(0,100);
-y1 = polyval(p,x1);
-figure
-plot(x,y,'o')
-hold on
-plot(x1,y1)
-hold off
+k = pinv(p)
+yn_t = yn'
+%coeff = k.*yn_t
 
-g = 0.0001*x.^2 - 0.0092*x + 0.8668;
+%plot(xn, yn, 'o')
+  
+%[p,S] = polyfit(x,y,n)
+
+%x1 = linspace(0,100);
+%y1 = polyval(p,x1);
+%figure
+%plot(x,y,'o')
+%hold on
+%plot(x1,y1)
+%hold off
+
+%g = 0.0001*x.^2 - 0.0092*x + 0.8668;
 
 % Erreur quadratique
-E = sum((g-y).^2)
-E2 = (g-y)'*(g-y);
+%E = sum((g-y).^2)
+%E2 = (g-y)'*(g-y);
 
 % RMSE
-err_rms = sqrt(mean((g-y).*(g-y)))
+%err_rms = sqrt(mean((g-y).*(g-y)))
 
 %% Coussin-trampoline
 
